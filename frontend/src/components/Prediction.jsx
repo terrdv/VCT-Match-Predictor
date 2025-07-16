1e6a3bf54610318b86a8e45cf9d7cf3582cdc266
terrdv <terrdv7@gmail.com>
2025-07-16 16:24:35 -0700

    api link

--- frontend/src/components/Prediction.jsx
+++ frontend/src/components/Prediction.jsx
@@ -18,7 +18,7 @@ function Prediction({ team1, team2, result, onReset }) {
                 <div className="teams-comparison">
                     <div className={`team-result ${result.team1_win_prediction ? 'winner' : 'loser'}`}>
                         <img 
-                            src={`http://localhost:5000${team1["Image Path"]}`} 
+                            src={`https://vct-match-predictor-api.onrender.com/api${team1["Image Path"]}`} 
                             alt={team1["Team"]}
                             className="team-logo"
                         />
@@ -35,7 +35,7 @@ function Prediction({ team1, team2, result, onReset }) {
                     
                     <div className={`team-result ${!result.team1_win_prediction ? 'winner' : 'loser'}`}>
                         <img 
-                            src={`http://localhost:5000${team2["Image Path"]}`} 
+                            src={`https://vct-match-predictor-api.onrender.com/api${team2["Image Path"]}`} 
                             alt={team2["Team"]}
                             className="team-logo"
                         />
